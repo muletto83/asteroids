6345cbfd5ad477eb132d1d5739ab8e921bb6abcc
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-25 17:28:29 -0400

    CH2-L2: game loop

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_SPAWN_RATE
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
