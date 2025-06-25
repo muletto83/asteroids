105c522297dc5457f50c2751727620ccf063edb4
muletto83 <19336827+muletto83@users.noreply.github.com>
2025-06-25 17:41:04 -0400

    CH2-L3: FPS

--- main.py
+++ main.py
@@ -7,13 +7,23 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MIN_
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    pygame.display.set_caption("Asteroid Game")
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+        #At the end of each iteration of the game loop,
+        # call the .tick() method, and pass it 60.
+        # It will pause the game loop until
+        # 1/60th of a second has passed.
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
